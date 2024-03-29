fe364f6ac4fd629a2f56f35782e43dad206fcdb2
M Hamza Tariq <111647708+Hmmza-tariq@users.noreply.github.com>
2024-03-29 11:43:28 +0500

    changes

--- lib/detali.dart
+++ lib/detali.dart
@@ -16,59 +16,100 @@ class _DetailsState extends State<Details> {
       appBar: AppBar(
         title: Text(widget.product.name),
       ),
-      body: Column(
-        children: [
-          Image.asset(widget.product.image),
-          Container(
-              padding: const EdgeInsets.all(10),
-              decoration: const BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.all(Radius.circular(10.0)),
-              ),
-              child: Column(
-                children: [
-                  Text(widget.product.name,
-                      style: const TextStyle(fontSize: 20)),
-                  Text(widget.product.price.toString(),
-                      style: const TextStyle(
-                          fontSize: 20,
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.blueAccent,
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          child: Column(
+            children: [
+              Image.asset(widget.product.image),
+              Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  child: Column(
+                    children: [
+                      Text(widget.product.name,
+                          style: const TextStyle(fontSize: 20)),
+                      Text("Rs. ${widget.product.price.toString()}",
+                          style: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim."
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+              Container(
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 106, 136, 189),
+                    border: Border(
+                      top: BorderSide(width: 1.0, color: Colors.black),
+                      left: BorderSide(width: 1.0, color: Colors.black),
+                      right: BorderSide(width: 1.0, color: Colors.black),
+                      bottom: BorderSide(width: 1.0, color: Colors.black),
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blue),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Cart()));
+                      },
+                      child: const Text(
+                        "Add to Cart",
+                        style: TextStyle(
+                          fontSize: 16,
                           color: Colors.white,
-                          fontWeight: FontWeight.bold)),
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.",
-                      style: TextStyle(
-                        fontSize: 15,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              )),
-          Container(
-            height: 100,
-            color: const Color.fromARGB(255, 106, 136, 189),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const Cart()));
-                  },
-                  child: const Text("Add to Cart"),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.blue),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Cart()));
+                      },
+                      child: const Text(
+                        "checkout",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const Cart()));
-                  },
-                  child: const Text("checkout"),
-                ),
-              ],
-            ),
-          )
-        ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
